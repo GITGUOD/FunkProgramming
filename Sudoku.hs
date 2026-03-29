0c5d49648127774fefee2b64b73efe850b711e89
Tonny Huynh <tonnyhuynh02@gmail.com>
2026-03-29 16:23:36 +0200

    Assignment 1 in Sudoku lab 1 is finished

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,4 +1,5 @@
 module Sudoku where
+import Data.Char (digitToInt)
 rows = "ABCD"
 cols = "1234"
 containsElem :: Eq a => a-> [a]-> Bool
@@ -9,3 +10,16 @@ containsElem elem (x:xs)
 
 cross :: [a] -> [a] -> [[a]]
 cross xs ys = [[x, y] | x <- xs, y <- ys]
+
+replacePointsWithZeros :: String -> String
+replacePointsWithZeros = map (\char -> if char == '.' then '0' else char)
+
+-- Ger samma output som Cross
+crossSquare :: String -> String -> [String]
+crossSquare rows cols = [[row, col] | row <- rows, col <- cols]
+
+matrix :: [[String]]
+matrix = [[ [r,c] | c <- cols ] | r <- rows]
+
+parseBoard :: String -> [(String, Int)]
+parseBoard input = zip (crossSquare rows cols) (map digitToInt (replacePointsWithZeros input))
