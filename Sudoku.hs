45099440777f43d3394e5942f15f6ce489cd8130
Tonny Huynh <tonnyhuynh02@gmail.com>
2026-03-29 20:21:00 +0200

    Added the tasks to the main program

--- Sudoku.hs
+++ Sudoku.hs
@@ -23,3 +23,46 @@ matrix = [[ [r,c] | c <- cols ] | r <- rows]
 
 parseBoard :: String -> [(String, Int)]
 parseBoard input = zip (crossSquare rows cols) (map digitToInt (replacePointsWithZeros input))
+
+
+-- Assignment 2:
+
+-- Task 1
+rowUnits :: [[String]]
+rowUnits = [[r:c:[] | c <- cols] | r <- rows]
+
+colUnits :: [[String]]
+colUnits = [[r:c:[] | r <- rows] | c <- cols]
+
+boxUnits :: [[String]]
+boxUnits = [cross rs cs | rs <- ["AB","CD"], cs <- ["12","34"]]
+
+unitList :: [[String]]
+unitList = rowUnits ++ colUnits ++ boxUnits
+
+-- Task 2:
+
+filterUnitList :: String -> [[String]]
+filterUnitList square = [unit | unit <- unitList, containsElem square unit]
+
+-- Task 3:
+units :: [(String, [[String]])]
+units = [(sq, filterUnitList sq) | sq <- crossSquare rows cols]
+
+-- Task 4:
+
+foldList :: [[a]] -> [a]
+foldList = concat
+
+
+-- Task 5:
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates [] = []
+removeDuplicates (first:last)
+    | first `elem` last = removeDuplicates last
+    | otherwise   = first : removeDuplicates last
+
+-- Task 6
+
+peers :: [(String, [String])]
+peers = [(sq, removeDuplicates (filter (/= sq) (foldList (filterUnitList sq)))) | sq <- crossSquare rows cols]
